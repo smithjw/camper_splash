1c77a10ab55708326f32bd93a96ad70d4ab4b01a
James Smith <james@smithjw.me>
2016-12-18 21:53:10 +1100

    Modified LaunchAgent to LaunchDaemon so that it's executed with  privileges

--- Library/CasperSplash/caspersplash.launch.sh
+++ Library/CasperSplash/caspersplash.launch.sh
@@ -4,16 +4,21 @@ plist="/Library/LaunchAgents/io.fti.caspersplash.launch.plist"
 loggedInUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
 app="/Library/CasperSplash/CasperSplash.app"
 script="/Library/CasperSplash/enrolment.sh"
+doneFile="/Users/${loggedInUser}/Library/.CasperSplashDone"
 
 # Check if:
 # - CasperSplash binary exists (is fully installed)
 # - User is in control (not _mbusersetup)
 # - User is on desktop (Finder process exists)
 
-if [ -f "$app"/Contents/MacOS/CasperSplash ] && [ "$loggedInUser" != "_mbusersetup" ] && [ $(pgrep Finder | wc -l) -gt 0 ]; then
+if [ -f "$app"/Contents/MacOS/CasperSplash ] \
+	&& [ "$loggedInUser" != "_mbusersetup" ] \
+	&& [ $(pgrep Finder | wc -l) -gt 0 ] \
+	&& [ ! -f "${doneFile}" ]; then
 
     open -a "$app"
-	sudo ."$script"
+	."$script"
+	
 	# remove and uninstall the launchdaemon
 
     launchctl remove io.fti.caspersplash.launch
