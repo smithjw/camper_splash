09fc57eba7dfd9aa6c57bf7fba50cf26196b64b1
James Smith <james@smithjw.me>
2017-05-06 11:48:00 +1000

    initial commit, updating for new verion of SplashBuddy

---
+++ Library/SplashBuddy/enrolment.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+jamfbinary='/usr/bin/which jamf'
+
+echo "Pulling down FileVault 2 configuration"
+jamfbinary policy -trigger "requireFV2"
+
+echo "Installing CAmper Assets"
+jamfbinary policy -trigger "camperAssets"
+
+echo "Installing DockUtil"
+jamfbinary policy -trigger "installDockUtil"
+
+echo "Re-enabling LittleSnitch where it had been disabled"
+jamfbinary policy -trigger "enableLittleSnitch"
+
+echo "Installing VPN Client"
+jamfbinary policy -trigger "install-Viscosity"
+
+echo "Installing Zoom"
+jamfbinary policy -trigger "install-Zoom"
+
+echo "Installing Slack"
+jamfbinary policy -trigger "install-Slack"
+
+echo "Installing Google Chrome"
+jamfbinary policy -trigger "install-Google Chrome"
+
+echo "Installing Box Sync client"
+jamfbinary policy -trigger "install-Box Sync"
+
+echo "Installing Quip"
+jamfbinary policy -trigger "install-Quip"
+
+echo "Setting up CAmper's Dock"
+jamfbinary policy -trigger "setDock"
+
+echo "Updating Inventory"
+jamfbinary policy -trigger "updateInventory"
