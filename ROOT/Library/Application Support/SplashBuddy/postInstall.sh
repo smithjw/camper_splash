ebf55a52a50cd243706c7a47208b0a01d7c7b728
James Smith <james@smithjw.me>
2017-11-05 08:09:14 +1100

    Adding new version of SB and removing some cruft

--- ROOT/Library/Application Support/SplashBuddy/postInstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-touch /Users/Shared/.SplashBuddyDone
-
-echo "Logging user out to force FileVault Encryption"
-osascript -e 'tell application "System Events" to keystroke "q" using {command down, option down, shift down}'
